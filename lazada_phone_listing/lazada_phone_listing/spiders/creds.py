032b6d8f8cbed023bf5ffa00ae896688ba61bcfe
allifizzuddin89 <allifizzuddin@gmail.com>
2022-11-16 17:53:20 +0800

    Add message in creds module

--- lazada_phone_listing/lazada_phone_listing/spiders/creds.py
+++ lazada_phone_listing/lazada_phone_listing/spiders/creds.py
@@ -1,3 +1,4 @@
 # Please register an account at https://www.scraperapi.com/?fp_ref=allif-izzuddin-bin-abdullah73
 # Paste and replace '< >' with the provided API
+# There is free account for you to test the proxy
 API = '<>'
